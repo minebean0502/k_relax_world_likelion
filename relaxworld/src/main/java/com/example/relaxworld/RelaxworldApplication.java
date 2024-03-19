cf67370e5cc37a75f020a28f12e712809aaad595
minebean0502 <no4323@gmail.com>
2024-03-19 13:48:04 +0900

    0319-1348

---
+++ relaxworld/src/main/java/com/example/relaxworld/RelaxworldApplication.java
@@ -0,0 +1,13 @@
+package com.example.relaxworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelaxworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelaxworldApplication.class, args);
+	}
+
+}
